c1834bb3b238985ea2f976a2975c3d4cd6bb707d
Zolorick <160130723+Zolorick@users.noreply.github.com>
2024-08-23 11:20:22 -0300

    tigre é elefante

---
+++ 4 semestre/POO/ex 1 scola/ex 1 scola/aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_1_scola
+{
+    internal class aluno : pessoa
+    {
+        private int ano_escolar;
+        private string periodo;
+        private int turma;
+
+        public bool assinar_presenca(bool presenca) 
+        {
+            
+            Console.WriteLine("Voce estava em aula hoje?");
+            Console.WriteLine("1 - sim");
+            Console.WriteLine("2 - nao");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "1") 
+            { 
+                presenca = true; 
+            }
+            else
+            {
+                 presenca = false;
+            }
+
+
+            return presenca;
+        }
+    }
+}
---
+++ 4 semestre/POO/ex 1 scola/ex 1 scola/professor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_1_scola
+{
+    internal class professor:pessoa
+    {
+        private string materia;
+        private float carga_horaria;
+        public bool assinar_presenca(bool presenca)
+        {
+
+            Console.WriteLine("Voce estava em aula hoje?");
+            Console.WriteLine("1 - sim");
+            Console.WriteLine("2 - nao");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "1")
+            {
+                presenca = true;
+            }
+            else
+            {
+                presenca = false;
+            }
+
+
+            return presenca;
+        }
+    }
+}
