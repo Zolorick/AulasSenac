04835b01b454448711eb100982b3f1c04a8e0994
Zolorick <160130723+Zolorick@users.noreply.github.com>
2024-08-23 08:25:36 -0300

    arrumando lugares

---
+++ 3 semestre/ADO/ADO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.Design;
+using System.IO;
+using ADO;
+
+Classe1 metodos = new Classe1();
+as_opçoes_menu acoes = new as_opçoes_menu();
+Arq arq = new Arq();
+
+
+int MenuOpcao, JogadaJogador, JogadaBot;
+
+MenuOpcao = metodos.Menu();
+
+switch (MenuOpcao) 
+{ 
+    case 1:
+        acoes.IniciarJogo();
+    break;
+
+    case 2:
+        acoes.ExibirResumo();
+    break;
+
+    case 3:
+        acoes.Sair();
+    break;
+}
+
+JogadaJogador = 1;
+JogadaBot = 2;
+
+switch (JogadaJogador, JogadaBot) 
+{
+    case (1,1):
+
+    break;
+}
