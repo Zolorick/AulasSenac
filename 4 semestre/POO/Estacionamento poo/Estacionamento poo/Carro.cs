22d73d4b4b876c0a7fdf61081be51d379f61619a
Zolorick <160130723+Zolorick@users.noreply.github.com>
2024-09-13 10:32:31 -0300

    Estacionamento

---
+++ 4 semestre/POO/Estacionamento poo/Estacionamento poo/Carro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento_poo
+{
+    public class Carro
+    {
+        public int ticket { get; private set; }
+        public Carro(int ordem) 
+        {
+            this.ticket = ordem;
+        }
+
+    }
+}
---
+++ 4 semestre/POO/Estacionamento poo/Estacionamento poo/UI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento_poo
+{
+    public class UI
+    {
+        public void Opcoes() 
+        {
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - Estacionar novo carro");
+            Console.WriteLine("2 - Sair com um carro");
+            Console.WriteLine("3 - Ver carros estacionados");
+            Console.WriteLine("4 - Encerrar o dia");
+        }
+        public void Linha() 
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 15; i++) 
+            {
+                Console.Write("*_* ");
+            }
+            Console.WriteLine();
+        }
+
+        public void LinhaSimples()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
