8e870721dfaa71adaf39a1fdc29adaac0894cfda
Zolorick <160130723+Zolorick@users.noreply.github.com>
2024-11-19 11:03:46 -0300

    classe emprestimo

---
+++ 4 semestre/POO/Ex Claudio2/Ex Claudio 2 Ex 4/Emprestimo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Claudio_2_Ex_4
+{
+    public class Emprestimo
+    {
+        private Livro liv;
+        private Aluno alun;
+        private DateTime dataEmprestimo = DateTime.Now;
+        private DateTime dataDevolução;
+      
+        public Livro Liv 
+        { get => liv; private set => liv = value; }
+        public Aluno Alun 
+        { get => alun; private set => alun = value; }
+
+        public Emprestimo(Livro liv, Aluno alun)
+        {
+            this.liv = liv;
+            this.alun = alun;
+            this.liv.Emprestimo = true;
+            dataDevolução = dataEmprestimo.AddDays(7);
+            Console.WriteLine($"Emprestando");
+        }
+        public void Devolver() 
+        { }
+
+        public void StatusEmprestimo() 
+        { }
+    }
+}
--- 4 semestre/POO/Ex Claudio2/Ex Claudio 2 Ex 4/Program.cs
+++ 4 semestre/POO/Ex Claudio2/Ex Claudio 2 Ex 4/Program.cs
@@ -1,3 +1,6 @@
 ﻿using Ex_Claudio_2_Ex_4;
 
 Livro livro = new Livro();
+Aluno aluno = new Aluno();
+
+Emprestimo empre = new Emprestimo(livro,aluno);
