d2c62c26273bfddf50e90eace6301ea201af8a9c
Zolorick <160130723+Zolorick@users.noreply.github.com>
2025-03-06 08:11:57 -0300

    so olhando

--- 5 semestre/dados/MasterKitchen2/MasterKitchen2/obj/Debug/net8.0/MasterKitchen2.AssemblyInfo.cs
+++ 5 semestre/dados/MasterKitchen2/MasterKitchen2/obj/Debug/net8.0/MasterKitchen2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterKitchen2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc765b68f80017c5ca803fb923c0cccd16675dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterKitchen2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterKitchen2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
